a30c539dab9ac066c4d43d73caf83899f521fd40
David Hackro <hackro91@gmail.com>
2017-03-05 01:53:50 -0600

    Creation Project and add dependencies

---
+++ app/src/main/java/tutorials/hackro/com/gallery/HomeActivity.java
@@ -0,0 +1,13 @@
+package tutorials.hackro.com.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
